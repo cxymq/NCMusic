b6a94271126adeb81d3ff92e5c280386e9a42891
nazimai <809670160@qq.com>
2024-09-26 15:29:52 +0800

    增加桌面组件

---
+++ NCWidget/NCWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NCWidgetBundle.swift
+//  NCWidget
+//
+//  Created by nazimai on 2024/9/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NCWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NCWidget()
+    }
+}
